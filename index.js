f9877c12a531a5f69ae5f133b60690a98595a886
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2022-03-20 15:58:24 +0200

    returned to res end

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
         console.log(data)
         res.setHeader('Content-Type', 'application/json');
-        res.sendStatus(200).end(data);
+        res.end(data);
     })
 });
 
@@ -14,4 +14,4 @@ const server = app.listen(process.env.PORT || 3000, () => {
     const host = server.address().address;
     const port = server.address().port;
     console.log(host, port)
-})
+});
