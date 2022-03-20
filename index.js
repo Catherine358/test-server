057fafe0665e68e74e4d557c4c132846fd4aca9d
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2022-03-20 17:21:18 +0200

    added cors package

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const fs = require('fs');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
