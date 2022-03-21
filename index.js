1a13eb1c24f081e2318c0660813e2b427e06c839
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2022-03-21 09:30:03 +0200

    added hives data to json and get by id

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/:hiveId', (req, res) => {
+    fs.readFile(__dirname + '/' + 'users.json', 'utf8', (err, data) => {
+        console.log(data)
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(req.body);
+    })
+});
+
 const server = app.listen(process.env.PORT || 8081, () => {
     const host = server.address().address;
     const port = server.address().port;
